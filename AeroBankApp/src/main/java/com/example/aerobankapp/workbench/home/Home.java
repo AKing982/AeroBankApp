0df247c7d5c2b6b6b13256ac39532e2023e3bc7c
AKing982 <59631931+AKing982@users.noreply.github.com>
2023-10-08 17:11:27 -0600

    Added Home

---
+++ AeroBankApp/src/main/java/com/example/aerobankapp/workbench/home/Home.java
@@ -0,0 +1,13 @@
+package com.example.aerobankapp.workbench.home;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Home extends Application
+{
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+
+    }
+}
