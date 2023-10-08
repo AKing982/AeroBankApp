ac0afbe4695ecd94bebe49c338dd2e714f668a8e
AKing982 <59631931+AKing982@users.noreply.github.com>
2023-10-08 16:35:10 -0600

    initial commit

---
+++ AeroBankApp/src/main/java/com/example/aerobankapp/AeroBankAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.aerobankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeroBankAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AeroBankAppApplication.class, args);
+    }
+
+}
---
+++ AeroBankApp/src/test/java/com/example/aerobankapp/AeroBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aerobankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeroBankAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
