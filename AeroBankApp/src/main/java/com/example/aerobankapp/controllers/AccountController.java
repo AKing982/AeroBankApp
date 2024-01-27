4305570a53278a302fd85dc921138eee31573b89
AKing982 <59631931+AKing982@users.noreply.github.com>
2024-01-26 20:23:54 -0700

    changes

--- AeroBankApp/src/main/java/com/example/aerobankapp/controllers/AccountController.java
+++ AeroBankApp/src/main/java/com/example/aerobankapp/controllers/AccountController.java
@@ -2,13 +2,15 @@ package com.example.aerobankapp.controllers;
 
 import com.example.aerobankapp.entity.AccountEntity;
 import com.example.aerobankapp.services.AccountDAOImpl;
+import com.example.aerobankapp.workbench.utilities.response.AccountResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/accounts", method = RequestMethod.GET)
@@ -29,6 +31,22 @@ public class AccountController
     public ResponseEntity<?> getUserAccounts(@PathVariable String user)
     {
         List<AccountEntity> accountEntities = accountDAO.findByUserName(user);
+        List<AccountResponse> accountResponseList = getAccountResponseList(accountEntities);
+
         return ResponseEntity.ok(accountEntities);
+    }
+
+    private List<AccountResponse> getAccountResponseList(List<AccountEntity> entityList, BigDecimal pending, BigDecimal available)
+    {
+        List<AccountResponse> accountResponseList = new ArrayList<>();
+        for(AccountEntity entity : entityList)
+        {
+            BigDecimal balance = entity.getBalance();
+            String acctCode = entity.getAccountCode();
+            AccountResponse accountResponse = new AccountResponse(acctCode, balance, pending, available);
+            accountResponseList.add(accountResponse);
+        }
+
+        return accountResponseList;
     }
 }
