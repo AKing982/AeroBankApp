648e05201aedd9c73780e2c5d11d4c36beda94fc
AKing982 <59631931+AKing982@users.noreply.github.com>
2024-02-07 19:38:05 -0700

    changes

--- AeroBankApp/src/main/java/com/example/aerobankapp/scheduler/jobs/DepositJob.java
+++ AeroBankApp/src/main/java/com/example/aerobankapp/scheduler/jobs/DepositJob.java
@@ -3,12 +3,18 @@ package com.example.aerobankapp.scheduler.jobs;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 public class DepositJob implements Job
 {
-
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        String depositDetails = "details";
+
+        // Send message to RabbitMQ
 
     }
 }
