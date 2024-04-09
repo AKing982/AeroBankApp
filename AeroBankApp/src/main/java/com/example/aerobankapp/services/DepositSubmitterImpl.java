9ea9f675ce60aca0a6a2543647d14ba35e0d21ea
AKing982 <59631931+AKing982@users.noreply.github.com>
2024-04-09 12:50:47 -0600

    added depositScheduler

--- AeroBankApp/src/main/java/com/example/aerobankapp/services/DepositSubmitterImpl.java
+++ AeroBankApp/src/main/java/com/example/aerobankapp/services/DepositSubmitterImpl.java
@@ -3,6 +3,7 @@ package com.example.aerobankapp.services;
 import com.example.aerobankapp.dto.DepositDTO;
 import com.example.aerobankapp.entity.DepositsEntity;
 import com.example.aerobankapp.repositories.DepositRepository;
+import com.example.aerobankapp.scheduler.DepositScheduler;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +15,15 @@ import org.springframework.stereotype.Service;
 public class DepositSubmitterImpl extends AbstractTransactionSubmitter<DepositDTO, DepositsEntity>
 {
     private final DepositService depositService;
+    private final DepositScheduler depositScheduler;
 
     @Autowired
     public DepositSubmitterImpl(DepositService depositService,
+                                DepositScheduler depositScheduler,
                                 FinancialEncryptionService financialEncryptionService){
         super(financialEncryptionService);
         this.depositService = depositService;
+        this.depositScheduler = depositScheduler;
     }
 
     @Override
